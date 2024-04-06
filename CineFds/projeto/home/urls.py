31d81481935cac4160b351fcf958e16620a61d13
Júlia Sales Novais <142419446+julsales@users.noreply.github.com>
2024-04-06 17:48:38 -0300

    Funcionou

---
+++ CineFds/projeto/home/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path, include
+from django.contrib import admin
+from . import views
+from home.views import *
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path("",views.home,name="home"),
+    path('cart/', views.cart, name='cart'),
+     path('remove_cart_item/<cart_item_uid>', remove_cart_item, name='remove_cart'),
+    path('add_cart/<movie_uid>', add_cart , name="add-cart"),
+    path("login",views.login_page,name="login"),
+    path('register/', views.register_page, name="register"),
+    path("admin/", admin.site.urls),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
