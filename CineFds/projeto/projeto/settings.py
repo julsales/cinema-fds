b2db587791469a72b8daf29bec1570ffd0e3f16b
Júlia Sales Novais <142419446+julsales@users.noreply.github.com>
2024-04-07 13:37:55 -0300

    Adicionei requirements.txt e +mudanças de html

--- CineFds/projeto/projeto/settings.py
+++ CineFds/projeto/projeto/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    "whitenoise.runserver_nostatic",
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'projeto.urls'
