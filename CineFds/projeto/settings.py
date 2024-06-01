c73277cb015ea88147de13c1a77920f9b23d1e64
Júlia Sales Novais <142419446+julsales@users.noreply.github.com>
2024-06-01 18:01:16 -0300

    mudança nas settings

--- CineFds/projeto/settings.py
+++ CineFds/projeto/settings.py
@@ -112,11 +112,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = os.environ.get('DJANGO_STATIC_URL', "/static/")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, 'static'),
-)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')
+MEDIA_URL = '/images/'
+
+STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'images')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
