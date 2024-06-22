44534dc3e18e45a3e4db24947691df5a81cad996
drakonbr <161368150+ChrisDrakon@users.noreply.github.com>
2024-06-22 00:30:22 -0300

    .

--- CineFds/home/models.py
+++ CineFds/home/models.py
@@ -47,7 +47,3 @@ class Comida(models.Model):
     def __str__(self):
         return self.nome
 
-class MovieRatingUS(models.Model):
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    nota = models.IntegerField()
